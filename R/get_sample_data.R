82f569086d15df3e759492853e0ffc24aff51aec
Robert B. Young <robertyoung3@gmail.com>
2020-06-08 08:56:38 -0400

    tried to use double curlies in get_elemental_ratios but reverted back

--- R/get_sample_data.R
+++ R/get_sample_data.R
@@ -16,35 +16,37 @@
 #' If the form of the files exported by PetroOrg changes, this function will
 #' need revisions.
 #'
-#' @param target_file a character string containing the filename and path for
+#' @param file a character string containing the filename and path for
 #'   the Excel file created by PetroOrg&copy after molecular formula assignment
 #' @param ion_technique a character string containing the MS ionization
 #'   technique used (default = NegESI)
 #' @param element_list a character vector containing the elements that were used
 #'   for molecular formula assignments (default = C, H, N, O, S)
 #'
+#' @importFrom rlang .data
+#'
 #' @return sample_data a named list containing file and sample information, MS
 #'   parameter information, a tibble containing the MS data for all assigned
 #'   elemental compositions, and a tibble containing the MS data for all
 #'   detected ions that could not be assigned formulas ("no hits")
 #' @export
-get_sample_data <- function(target_file = file.choose(), ion_technique = "NegESI",
+get_sample_data <- function(file = file.choose(), ion_technique = "NegESI",
                             element_list = c("C", "H", "N", "O", "S")) {
-  sample_data <- list(sample_name = tools::file_path_sans_ext(basename(target_file)),
-                      file_name = basename(target_file),
-                      orig_file_path = target_file,
+  sample_data <- list(sample_name = tools::file_path_sans_ext(basename(file)),
+                      file_name = basename(file),
+                      orig_file_path = file,
                       ion_technique = ion_technique,
                       element_list = element_list)
 
   ## (1) read no hits data
-  sample_data[["no_hits"]] <- target_file %>%
+  sample_data$no_hits <- file %>%
     readxl::read_excel(sheet = "No Hit", col_names = FALSE, skip = 2) %>%
     dplyr::select(2, 4)
-  colnames(sample_data[["no_hits"]]) <- c("mz", "rel_abund")
+  colnames(sample_data$no_hits) <- c("mz", "rel_abund")
 
   ## (2) read assigned formula data
   #### (a) get sheet names
-  sheetnames <- readxl::excel_sheets(target_file)
+  sheetnames <- readxl::excel_sheets(file)
 
   #### (b) subset to exclude isotopes and sheetnames with 3 or more consecutive letters (data summaries)
   sheetnames <- sheetnames[stringr::str_detect(sheetnames, "13C|34S|[[:alpha:]]{3,}", negate = TRUE)]
@@ -54,14 +56,14 @@ get_sample_data <- function(target_file = file.choose(), ion_technique = "NegESI
 
   #### (d) read and clean data by mapping through sheet names
   temp <- sheetnames %>%
-    purrr::map_dfr(~readxl::read_excel(path = target_file, sheet = ., col_names = TRUE, skip = 2), .id = "hetero_class") %>%
+    purrr::map_dfr(~readxl::read_excel(path = file, sheet = ., col_names = TRUE, skip = 2), .id = "hetero_class") %>%
     janitor::clean_names() %>%
     dplyr::select(-tidyselect::one_of("x1", "exp_m_z", "signal2noise", "dbe", "molecular_formula"),
                   -tidyselect::ends_with("_c")) %>%
-    dplyr::rename(mz = recal_m_z,
-                  theor_mz = theor_mass,
-                  ppm_error = error,
-                  rel_abund = rel_abundance)
+    dplyr::rename(mz = .data$recal_m_z,
+                  theor_mz = .data$theor_mass,
+                  ppm_error = .data$error,
+                  rel_abund = .data$rel_abundance)
 
   #### (e) rename element columns with first element in prior column
   for (i in element_list) {
@@ -73,19 +75,19 @@ get_sample_data <- function(target_file = file.choose(), ion_technique = "NegESI
     dplyr::select(-tidyselect::matches("x[[:digit:]]+"))
 
   #### (g) generate molecular formulas & strip empty elements
-  temp[["chem_formula"]] <- ""
+  temp$chem_formula <- ""
   for (i in element_list) {
-    temp[["chem_formula"]] <- temp[["chem_formula"]] %>%
+    temp$chem_formula <- temp$chem_formula %>%
       stringr::str_c(i, temp[[i]]) %>%
       stringr::str_replace(stringr::str_c(i, "0"), "")
   }
 
   #### (h) reorder columns
   temp <- temp %>%
-    dplyr::select(hetero_class, chem_formula, tidyselect::everything())
+    dplyr::select(.data$hetero_class, .data$chem_formula, tidyselect::everything())
 
   #### (i) assign temp to sample_data
-  sample_data[["assigned_formulas"]] <- temp
+  sample_data$assigned_formulas <- temp
 
   ## (3)return named list
   return(sample_data)
