f8e111d53b5e79dfe2a096794f3a1fcbb43c5d7a
Robert B. Young <robertyoung3@gmail.com>
2020-06-17 10:26:38 -0400

    created more flexible x axis in plot_mass_spectrum

--- R/plot_mass_spectrum.R
+++ R/plot_mass_spectrum.R
@@ -1,11 +1,15 @@
 #' plot_mass_spectrum
 #'
-#' This function plots the sample mass spectrum.The maximum x value can be
-#' specified to produce figures for multiple samples on the same scale.
+#' This function plots the sample mass spectrum.The maximum mz value can be
+#' specified to produce figures for multiple samples on the same scale. Also,
+#' the minimum and maximum mz values can be specified to plot a specific region
+#' of the mass spectrum.
 #'
 #' @param data a tibble containing the assigned molecular formulas
 #' @param plot_title a character string containing the sample name
-#' @param max_x a numeric containing the maximum x value to plot (default =
+#' @param min_x a numeric containing the minimum mz value to plot (default =
+#'   NULL)
+#' @param max_x a numeric containing the maximum mz value to plot (default =
 #'   NULL)
 #'
 #' @importFrom rlang .data
@@ -13,12 +17,17 @@
 #'
 #' @return
 #' @export
-plot_mass_spectrum <- function(data, plot_title, max_x = NULL) {
-  if (is.null(max_x)) {
-    x_ticks <- pretty(data$mz)
+plot_mass_spectrum <- function(data, plot_title, min_x = NULL, max_x = NULL) {
+  if (!is.null(min_x) & !is.null(max_x)) {
+    x_ticks <- pretty(min_x:max_x)
+  } else if (is.null(min_x) & !is.null(max_x)) {
+    x_ticks <- pretty(min(data$mz):max_x)
+  } else if (!is.null(min_x) & is.null(max_x)) {
+    x_ticks <- pretty(min_x:max(data$mz))
   } else {
-    x_ticks <- pretty(100:max_x)
+    x_ticks <- pretty(data$mz)
   }
+
   ggplot2::ggplot(data, aes(x = .data$mz,
                             ymin = 0, ymax = .data$rel_abund)) +
     ggplot2::geom_linerange(size = 0.1) +
